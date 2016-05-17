9f9634e0fd862c03703744a5d60a053ae84d85c3
Michael Chavez <bigbassroller@gmail.com>
2016-05-16 21:57:37 -0700

    added gulp sass build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,9 @@ import {Component, Directive, ElementRef, Renderer} from '@angular/core';
 import {RouteConfig, ROUTER_DIRECTIVES} from '@angular/router-deprecated';
 import {Http} from '@angular/http';
 import {XLarge} from './components/x-large.component.ts';
-import {About} from './about/about.component';
 import {Home} from './home/home.component';
+import {About} from './about/about.component';
+import {Portfolio} from './portfolio/portfolio.component';
 
 @Component({
   selector: 'app', // <app></app>
@@ -11,13 +12,14 @@ import {Home} from './home/home.component';
     ...ROUTER_DIRECTIVES,
     XLarge
   ],
-  styleUrls: ['src/app/app.component.css'],
+  styleUrls: ['dist/client/so-much-style.min.css'],
   templateUrl: 'src/app/app.component.html'
 })
 @RouteConfig([
   { path: '/', component: Home, name: 'Home', useAsDefault: true },
   { path: '/home', component: Home, name: 'Home' },
   { path: '/about', component: About, name: 'About' },
+  { path: '/portfolio', component: Portfolio, name: 'Portfolio' },
   { path: '/**', redirectTo: ['Home'] }
 ])
 export class App {
@@ -34,6 +36,11 @@ export class App {
     }, 10);
 
     this.http.get('/app.json')
+      .subscribe(res => {
+        this.data = res.json();
+      });
+
+     this.http.get('/about.json')
       .subscribe(res => {
         this.data = res.json();
       });
